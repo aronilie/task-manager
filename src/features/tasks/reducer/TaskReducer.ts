6e85cf31ed78f8cf788328ac08acac2501f4f168
Aron Ilie <aronilie.code@gmail.com>
2022-08-09 19:13:21 +0200

    Created actionCreators

--- src/features/tasks/reducer/TaskReducer.ts
+++
@@ -1,8 +0,0 @@
-import { createReducer } from "@reduxjs/toolkit";
-import { Task } from "../models/Task";
-
-export const initialState: Task[] = [];
-
-export const taskToDoReducer = createReducer(initialState, (builder) => {
-  builder.addDefaultCase((state: Task[]) => [...state]);
-});
